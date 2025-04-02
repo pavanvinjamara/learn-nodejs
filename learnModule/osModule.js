da30c4c9f36ecf296f0bee947eb380e0c814c473
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-04-02 23:55:38 +0530

    upto path module compeleted

---
+++ learnModule/osModule.js
@@ -0,0 +1,20 @@
+// Modules
+
+// Normally we import modules using require statement
+// Os Module
+
+// importing os modules using require
+const os = require('os');
+
+// To know operating system of our os (windows) we use type method
+console.log(os.type());  
+// Os version
+console.log(os.version());  
+// free memory Space
+console.log(os.freemem());
+// CPU Status  
+console.log(os.cpus());  
+
+
+
+
---
+++ learnModule/pathModule.js
@@ -0,0 +1,15 @@
+
+// To know current folder and file
+console.log(__dirname); // for folder
+console.log(__filename); // for file
+
+// Path module is used for to interact with files and folders
+// import the path module using require statement
+const path = require('path');
+
+console.log(path.dirname(__filename)); // to get folder path (C:\Users\V Srinath\learn-nodejs\learn-nodejs\learnModule)
+console.log(path.basename(__filename)); // file name with extenstio (pathModule.js)
+console.log(path.extname(__filename)); // To know dot extention (.js)
+
+// To get all the data of file  in object
+console.log(path.parse(__filename));
