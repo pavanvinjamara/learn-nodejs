53f124b8c9dfe78356b1b1954cdfd578b6c98c8c
pavanvinjamara <pavanvinjamara323@gmail.com>
2025-04-28 22:29:53 +0530

    mongoose connected

--- MongoDB/apiIntegration/server.js
+++ MongoDB/apiIntegration/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const dotEnv =  require('dotenv');
-const mongoose = require('mongooose');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000
 
+dotEnv.config();
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("MongoDB connected successfully")
+})
+.catch((err)=>{
+    console.log(err)
+})
 app.listen(PORT, ()=>{
     console.log(`server started @ ${PORT}`);
 })
